2b8ca64efae13021519ef09ecd29b9ef6b1ee24a
Magicflyingcow <47694609+Magicflyingcow@users.noreply.github.com>
2019-02-17 18:02:02 +0000

    Update and rename lastfm(good)final.py to lastfm(mainstream.ghan.nl).py

--- lastfm(mainstream.ghan.nl).py
+++ lastfm(mainstream.ghan.nl).py
@@ -4,7 +4,7 @@ import matplotlib.dates as mdates
 from matplotlib.dates import DateFormatter
 
 artists='Artists';albums='Albums';tracks='Tracks'
-name='hedphylem'
+name='USERNAME'
 
 
 
@@ -17,33 +17,32 @@ def f(number,subject,days):
     
     with open(f"{name}.csv",'r',encoding='utf-8') as file:
         
-        csv_reader=csv.reader(file)
+        csv_reader=csv.reader(file,delimiter=',',quotechar='"')
+        
         next(csv_reader)
         t=int(time.time())
         t1=time.time()
-        if subject=='Artists':
-            column=2
-        if subject=='Albums':
-            column=4
-        if subject=='Tracks':
-            column=6
+        print(f"Scanning {subject}...")
+        
+        if subject=='Artists': column=2
+        if subject=='Albums': column=4
+        if subject=='Tracks': column=6
     
         for line in csv_reader:
             if (t-int(line[0]))>secs and secs!=0:
                 break
             
             n+=1
-
-            if line[column] not in names:
+            if line[column]=='': print(line);next(csv_reader)# will show albumless tracks
+            elif line[column] not in names:
                 names.append(line[column])
                 nums.append(1)
-               #if subject=='Tracks':
                     
             else:
                 nums[names.index(line[column])]+=1
                 
         t2=time.time()
-        print(t2-t1)
+        print(f"{subject} scanned in {t2-t1} seconds")
 
         
         for i in range(len(nums)):
@@ -56,19 +55,21 @@ def f(number,subject,days):
         for j in range(number):
             x=[];y=[];p=0
             file.seek(0)
+            next(csv_reader)
             for line2 in csv_reader:
-
-                #print(data);print(line)
-                if data[j][1] in line2[column]:
+                
+                if data[j][1]==line2[column]:
+                    
                     if (t-int(line2[0]))>secs and secs!=0:
                         break
                     p+=1
+                    
                     x.insert(0,int(line2[0]))
-                    #print(x)
+                    
                     y.append(p)
-                #print(line2)
+                
             
-            #print(x)
+            
             x.append(int(time.time()))
             y.append(p)
             x=[datetime.fromtimestamp(val) for val in x]
@@ -86,7 +87,7 @@ def f(number,subject,days):
     plt.legend(fontsize=5,loc=2)    #size of legend font
     plt.grid(0,linestyle='--',alpha=0.5)
     plt.xlabel('Date')
-    plt.ylabel('Scrobbles')
+    plt.ylabel('Plays')
     startdate=datetime.utcfromtimestamp(int(line[0])).strftime('%Y-%m-%d')
     plt.title(f"{name}'s Top {number} {subject} Since {startdate}",fontsize=10)
     plt.savefig(f"{name}'s Top {number} {subject} Since {startdate}.png",dpi=800,bbox_inches='tight')
@@ -94,6 +95,3 @@ def f(number,subject,days):
     print(f"{totaltime2-plottime} seconds to plot data")
     print(f"{totaltime2-totaltime1} seconds overall")
     plt.clf()
-      
-
- 
